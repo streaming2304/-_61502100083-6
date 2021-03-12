1a9835a327e64b1b0f2e050ea7f589c12edb36e8
artit <615021000836@gmail.rmutk.ac.th>
2021-03-12 22:26:22 +0700

    1.5

--- table11.php
+++ table11.php
@@ -1,4 +1,14 @@
-<?php require_once("dbcon.php");?>
+<?php require_once("dbcon.php");
+if(isset($_GET['id'])){
+    $result = $conn-> query("DELETE FROM movies WHERE movie_id=".$_GET['id']);
+    if($result){
+        header("Location table11.php");
+    }else{
+        echo"?????????????????";
+    }
+  }
+
+?>
 <form action="." method="get">
 <input type="text" name="search" id="search" placehotlder="ค้นหา">
 <button type="submit" name="click">ค้นหา</button>
@@ -11,6 +21,7 @@
     <th>เวลาที่ฉาย</th>
     <th>ชื่อผู้ใช้งาน</th>
     <th>pin</th>
+    <th>จัดการ</th>
   </tr>
   <?php
 $sql = "SELECT * FROM movies";
@@ -28,7 +39,7 @@ $result = $conn ->query($sql);
   <td><?php echo $row['movie_time'];?></td>
   <td><?php echo $row['movie_user'];?></td>
   <td><?php echo $row['pin'];?></td>
-  <td>
+  <td><a href="?delete=1&id=<?php echo $row['movie_id'];?>">ลบ</a></td>
   
 
 </tr>
